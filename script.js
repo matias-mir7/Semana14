5f27bed784a2a4ceebd1b456efc228e2129a845b
matias-mir7 <gcmmir@gmail.com>
2023-10-04 09:57:14 -0300

    Responsive Design y Bootstrap

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var myCarousel = new bootstrap.Carousel(document.getElementById('textCarousel'), {
+        interval: 3000 // cantidad de milisegundos para cambiar el carucel
+    });
+});
